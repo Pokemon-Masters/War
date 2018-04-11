5e5de2f64438956541a3eccf3b172d9f13a52efd
Max Latoche <max.latoche@gmail.com>
2018-04-11 15:08:48 -0400

    working front end hello world test

---
+++ client/Components/GameBoard.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class GameBoard extends Component {
+    constructor(props) {
+        super();
+    }
+
+    render() {
+        return (
+            <div id="game-board">
+                test
+            </div>
+        )
+    }
+}
+
+export default GameBoard;
