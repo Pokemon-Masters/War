740301468108130918218944eaae8981289a05f6
jmw1493 <weissjmw@gmail.com>
2018-04-11 14:35:09 -0400

    skldjf

--- Server/server.js
+++ Server/server.js
@@ -2,12 +2,9 @@ const express = require('express');
 const app = express();
 // const scrapeController = require('./scraper');
 const Sequelize = require('sequelize');
-<<<<<<< HEAD
 const scrapeController = require('./scraper3');
 const dbController = require('./db');
-=======
 const uri = 'postgres://udeyhfsk:vgeewt-ITqXsidtAxEZd7rBqtkPT8CZ0@baasu.db.elephantsql.com:5432/udeyhfsk';
->>>>>>> 015e7fb6a68a2a39d297ed44ca472d859ca48c15
 
 // const sequelize = new Sequelize(uri, 'udeyhfsk', 'vgeewt-ITqXsidtAxEZd7rBqtkPT8CZ0');
 
@@ -20,7 +17,6 @@ const uri = 'postgres://udeyhfsk:vgeewt-ITqXsidtAxEZd7rBqtkPT8CZ0@baasu.db.eleph
 //     console.error('error with connection: ', err);
 //   });
 
-<<<<<<< HEAD
   // app.use(...)
   
 app.get('/', (req, res) => {
@@ -32,9 +28,7 @@ app.get('/scrape', scrapeController.getPokemon, scrapeController.getUrls);
 app.get('/generate-deck', dbController.generateDeck);
 
 // app.get('/', scrapeController.runScrapeMain, scrapeController.runScrapePhoto);
-=======
-app.get('/', scraper.runScrapeMain);
->>>>>>> 015e7fb6a68a2a39d297ed44ca472d859ca48c15
+// app.get('/', scraper.runScrapeMain);
 
 app.listen(3000, () => {console.log('server is running on port 3000')});
 
