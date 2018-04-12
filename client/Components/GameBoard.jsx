e1cd3bdc92e247be8cbd05eae0b2a4c93ce02bf8
Max Latoche <max.latoche@gmail.com>
2018-04-12 11:16:15 -0400

    partially completed slideable UI

--- client/Components/GameBoard.jsx
+++ client/Components/GameBoard.jsx
@@ -1,14 +1,279 @@
 import React, { Component } from 'react';
+import TeamQueue from './TeamQueue.jsx';
 
 class GameBoard extends Component {
     constructor(props) {
         super();
+
+        this.state = {
+            redLineup: [
+                {
+                    "name": "Bulbasaur",
+                    "number": 1,
+                    "level": 10,
+                    "queuePosition": 1,
+                    "type": [
+                        "Grass",
+                        "Poison"
+                    ],
+                    "pokemonUrl": "https://img.pokemondb.net/artwork/bulbasaur.jpg"
+                },
+                {
+                    "name": "Ivysaur",
+                    "number": 1,
+                    "level": 10,
+                    "queuePosition": 2,
+                    "type": [
+                        "Grass",
+                        "Poison"
+                    ],
+                    "pokemonUrl": "https://img.pokemondb.net/artwork/ivysaur.jpg"
+                },
+                {
+                    "name": "Venusaur",
+                    "number": 1,
+                    "level": 10,
+                    "queuePosition": 3,
+                    "type": [
+                        "Grass",
+                        "Poison"
+                    ],
+                    "pokemonUrl": "https://img.pokemondb.net/artwork/venusaur.jpg"
+                },
+                {
+                    "name": "Charmander",
+                    "number": 1,
+                    "level": 10,
+                    "queuePosition": 4,
+                    "type": [
+                        "Fire"
+                    ],
+                    "pokemonUrl": "https://img.pokemondb.net/artwork/charmander.jpg"
+                },
+                {
+                    "name": "Charmeleon",
+                    "number": 1,
+                    "level": 10,
+                    "queuePosition": 5,
+                    "type": [
+                        "Fire"
+                    ],
+                    "pokemonUrl": "https://img.pokemondb.net/artwork/charmeleon.jpg"
+                },
+                {
+                    "name": "Charizard",
+                    "number": 1,
+                    "level": 10,
+                    "queuePosition": 6,
+                    "type": [
+                        "Fire",
+                        "Flying"
+                    ],
+                    "pokemonUrl": "https://img.pokemondb.net/artwork/charizard.jpg"
+                },
+                {
+                    "name": "Squirtle",
+                    "number": 1,
+                    "level": 10,
+                    "queuePosition": 7,
+                    "type": [
+                        "Water"
+                    ],
+                    "pokemonUrl": "https://img.pokemondb.net/artwork/squirtle.jpg"
+                },
+                {
+                    "name": "Wartortle",
+                    "number": 1,
+                    "level": 10,
+                    "queuePosition": 8,
+                    "type": [
+                        "Water"
+                    ],
+                    "pokemonUrl": "https://img.pokemondb.net/artwork/wartortle.jpg"
+                },
+                {
+                    "name": "Blastoise",
+                    "number": 1,
+                    "level": 10,
+                    "queuePosition": 9,
+                    "type": [
+                        "Water"
+                    ],
+                    "pokemonUrl": "https://img.pokemondb.net/artwork/blastoise.jpg"
+                },
+                {
+                    "name": "Caterpie",
+                    "number": 1,
+                    "level": 10,
+                    "queuePosition": 10,
+                    "type": [
+                        "Bug"
+                    ],
+                    "pokemonUrl": "https://img.pokemondb.net/artwork/caterpie.jpg"
+                },
+                {
+                    "name": "Metapod",
+                    "number": 1,
+                    "level": 10,
+                    "queuePosition": 11,
+                    "type": [
+                        "Bug"
+                    ],
+                    "pokemonUrl": "https://img.pokemondb.net/artwork/metapod.jpg"
+                },
+                {
+                    "name": "Butterfree",
+                    "number": 1,
+                    "level": 10,
+                    "queuePosition": 12,
+                    "type": [
+                        "Bug",
+                        "Flying"
+                    ],
+                    "pokemonUrl": "https://img.pokemondb.net/artwork/butterfree.jpg"
+                },
+            ],
+            blueLineup: [
+                {
+                    "name": "Weedle",
+                    "number": 1,
+                    "level": 10,
+                    "queuePosition": 1,
+                    "type": [
+                        "Bug",
+                        "Poison"
+                    ],
+                    "pokemonUrl": "https://img.pokemondb.net/artwork/weedle.jpg"
+                },
+                {
+                    "name": "Kakuna",
+                    "number": 1,
+                    "level": 10,
+                    "queuePosition": 2,
+                    "type": [
+                        "Bug",
+                        "Poison"
+                    ],
+                    "pokemonUrl": "https://img.pokemondb.net/artwork/kakuna.jpg"
+                },
+                {
+                    "name": "Beedrill",
+                    "number": 1,
+                    "level": 10,
+                    "queuePosition": 3,
+                    "type": [
+                        "Bug",
+                        "Poison"
+                    ],
+                    "pokemonUrl": "https://img.pokemondb.net/artwork/beedrill.jpg"
+                },
+                {
+                    "name": "Pidgey",
+                    "number": 1,
+                    "level": 10,
+                    "queuePosition": 4,
+                    "type": [
+                        "Normal",
+                        "Flying"
+                    ],
+                    "pokemonUrl": "https://img.pokemondb.net/artwork/pidgey.jpg"
+                },
+                {
+                    "name": "Pidgeotto",
+                    "number": 1,
+                    "level": 10,
+                    "queuePosition": 5,
+                    "type": [
+                        "Normal",
+                        "Flying"
+                    ],
+                    "pokemonUrl": "https://img.pokemondb.net/artwork/pidgeotto.jpg"
+                },
+                {
+                    "name": "Pidgeot",
+                    "number": 1,
+                    "level": 10,
+                    "queuePosition": 6,
+                    "type": [
+                        "Normal",
+                        "Flying"
+                    ],
+                    "pokemonUrl": "https://img.pokemondb.net/artwork/pidgeot.jpg"
+                },
+                {
+                    "name": "Rattata",
+                    "number": 1,
+                    "level": 10,
+                    "queuePosition": 7,
+                    "type": [
+                        "Normal"
+                    ],
+                    "pokemonUrl": "https://img.pokemondb.net/artwork/rattata.jpg"
+                },
+                {
+                    "name": "Raticate",
+                    "number": 1,
+                    "level": 10,
+                    "queuePosition": 8,
+                    "type": [
+                        "Normal"
+                    ],
+                    "pokemonUrl": "https://img.pokemondb.net/artwork/raticate.jpg"
+                },
+                {
+                    "name": "Spearow",
+                    "number": 1,
+                    "level": 10,
+                    "queuePosition": 9,
+                    "type": [
+                        "Normal",
+                        "Flying"
+                    ],
+                    "pokemonUrl": "https://img.pokemondb.net/artwork/spearow.jpg"
+                },
+                {
+                    "name": "Fearow",
+                    "number": 1,
+                    "level": 10,
+                    "queuePosition": 10,
+                    "type": [
+                        "Normal",
+                        "Flying"
+                    ],
+                    "pokemonUrl": "https://img.pokemondb.net/artwork/fearow.jpg"
+                },
+                {
+                    "name": "Ekans",
+                    "number": 1,
+                    "level": 10,
+                    "queuePosition": 11,
+                    "type": [
+                        "Poison"
+                    ],
+                    "pokemonUrl": "https://img.pokemondb.net/artwork/ekans.jpg"
+                },
+                {
+                    "name": "Arbok",
+                    "number": 1,
+                    "level": 10,
+                    "queuePosition": 12,
+                    "type": [
+                        "Poison"
+                    ],
+                    "pokemonUrl": "https://img.pokemondb.net/artwork/arbok.jpg"
+                },
+            ],
+        }
     }
 
+    
+
     render() {
+
         return (
             <div id="game-board">
-                test
+                <TeamQueue className='red' team={'red'} lineup={this.state.redLineup} />
+                <TeamQueue className='blue' team={'blue'} lineup={this.state.blueLineup} />
             </div>
         )
     }
---
+++ client/Components/TeamQueue.jsx
@@ -0,0 +1,172 @@
+import React, { Component } from 'react';
+import Draggable from 'react-draggable';
+import PropTypes from 'prop-types';
+
+import PokemonCard from './PokemonCard.jsx';
+
+class TeamQueue extends Component {
+    constructor(props) {
+        super(props);
+        this.typeMap = {
+            grass: 'https://cdn.bulbagarden.net/upload/thumb/2/2e/Grass-attack.png/20px-Grass-attack.png',
+            fire: 'https://cdn.bulbagarden.net/upload/thumb/a/ad/Fire-attack.png/20px-Fire-attack.png',
+            water: 'https://cdn.bulbagarden.net/upload/thumb/1/11/Water-attack.png/20px-Water-attack.png',
+            lightning: 'https://cdn.bulbagarden.net/upload/thumb/0/04/Lightning-attack.png/20px-Lightning-attack.png',
+            fighting: 'https://cdn.bulbagarden.net/upload/thumb/4/48/Fighting-attack.png/20px-Fighting-attack.png',
+            psychic: 'https://cdn.bulbagarden.net/upload/thumb/e/ef/Psychic-attack.png/20px-Psychic-attack.png',
+            colorless: 'https://cdn.bulbagarden.net/upload/thumb/1/1d/Colorless-attack.png/20px-Colorless-attack.png',
+            darkness: 'https://cdn.bulbagarden.net/upload/thumb/a/ab/Darkness-attack.png/20px-Darkness-attack.png',
+            metal: 'https://cdn.bulbagarden.net/upload/thumb/6/64/Metal-attack.png/20px-Metal-attack.png',
+            dragon: 'https://cdn.bulbagarden.net/upload/thumb/8/8a/Dragon-attack.png/20px-Dragon-attack.png',
+            fairy: 'https://cdn.bulbagarden.net/upload/thumb/4/40/Fairy-attack.png/20px-Fairy-attack.png'
+        }
+        this.state = {
+            activeDrags: 0,
+            deltaPosition: {
+                x: 0, y: 0
+              },
+            types: null,
+            cardQueue: null,
+            pos: {
+                0: 302,
+                1: 604,
+                2: 906,
+                3: 1208,
+                4: 1510,
+                5: 1812,
+                6: 2114,
+                7: 2416,
+                8: 2718,
+                9: 3020,
+                10: 3322,
+                11: 3624,
+            },
+            isOccupied: [
+
+            ]
+        }
+        this.handleDrag = this.handleDrag.bind(this);
+        this.onStop = this.onStop.bind(this);
+    }
+
+    getTypes (type) {
+        const result = [];
+        for (let i = 0; i < type.length; i++) {
+            switch (type[i].toLowerCase()) {
+                case 'grass':
+                case 'bug':
+                    result.push(['grass', this.typeMap.grass]);
+                    break;
+                case 'fire':
+                    result.push(['fire', this.typeMap.fire]);
+                    break;
+                case 'water':
+                case 'ice':
+                    result.push(['water', this.typeMap.water]);
+                    break;
+                case 'electric':
+                    result.push(['lightning', this.typeMap.lightning]);
+                    break;
+                case 'fighting':
+                case 'rock':
+                case 'ground':
+                    result.push(['fightning', this.typeMap.fighting]);
+                    break;
+                case 'psychic':
+                case 'ghost':
+                    result.push(['psychic', this.typeMap.psychic]);
+                    break;
+                case 'normal':
+                case 'flying':
+                    result.push(['colorless', this.typeMap.colorless]);
+                    break;
+                case 'dark':
+                    result.push(['darkness', this.typeMap.darkness]);
+                    break;
+                case 'steel':
+                    result.push(['metal', this.typeMap.metal]);
+                    break;
+                case 'fairy':
+                    result.push(['fairy', this.typeMap.fairy]);
+                    break;
+                case 'dragon':
+                    result.push(['colorless', this.typeMap.colorless]);
+                    result.push(['dragon', this.typeMap.dragon]);
+                    break;
+                case 'poison':
+                    result.push(['grass', this.typeMap.grass]);
+                    result.push(['psychic', this.typeMap.psychic]);
+                    break;
+           }
+        }
+        return result;
+    }
+
+    handleDrag(e, ui) {
+        const {x, y} = this.state.deltaPosition;
+        console.log('ui',ui)
+        console.log('e', e)
+        console.log(ui.node.id)
+        this.setState({
+            deltaPosition: {
+                x: x + ui.deltaX,
+                y: y + ui.deltaY,
+            }
+        });
+    }
+    
+    onStop() {
+        this.setState({activeDrags: --this.state.activeDrags});
+    }
+    
+    orderCards() {
+        const cardQueue = [];
+        const dragHandlers = {
+            onDrag: this.handleDrag, 
+            onStop: this.onStop,
+            axis: 'y',
+            handle: ".handle",
+            grid: [0, 312]
+        };
+
+        for (let i = 0; i < this.props.lineup.length; i++) {
+            const types = this.getTypes(this.props.lineup[i].type)
+            cardQueue.push(
+                <Draggable key={i+this.props.team} id={i} {...dragHandlers}>
+                <div className="handle">
+                    <PokemonCard key={'pokemonCard'+i} pokemon={this.props.lineup[i]} types={types} />
+                </div>
+                </Draggable>
+            );
+        }
+
+        this.setState({
+            cardQueue
+        })
+    }
+
+    componentDidMount() {
+        this.orderCards();
+    }   
+    
+
+    render() {
+       
+        
+        
+
+        return (
+            <div className="team-queue" id={this.props.team}>  
+                {this.state.cardQueue}
+            </div>
+        )
+    }
+}
+
+TeamQueue.propTypes = {
+	// links: PropTypes.array.isRequired,
+	// current: PropTypes.string.isRequired,
+	// updateCurrent: PropTypes.func.isRequired
+}
+
+export default TeamQueue;
